fd71af1b1bd6817e94e0e0efa2dfd078c366f663
Mike Pennisi <mike@mikepennisi.com>
2017-12-28 21:34:36 -0500

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const test = require('tape');
+
+const isSemantic = require('..');
+
+const assert = (selector, expected) => {
+  if (isSemantic(selector) !== expected) {
+    throw new Error(
+      `Expected "${selector}" to be labeled ${expected ? '' : 'non-'}semantic, but it was not.`
+    );
+  }
+};
+
+test('tag names', (t) => {
+  [
+    'body', 'header', 'footer', 'nav', 'article', 'section'
+  ].forEach((selector) => assert(selector, true));
+
+  [
+    'div', 'span'
+  ].forEach((selector) => assert(selector, false));
+
+  t.end();
+});
+
+test('lists', (t) => {
+  [
+    'body, header, footer', 'nav, article, section'
+  ].forEach((selector) => assert(selector, true));
+
+  [
+    'div, span', 'body, div', 'span, header', 'nav, div, section'
+  ].forEach((selector) => assert(selector, false));
+
+  t.end();
+});
+
+test('simple descendents', (t) => {
+  [
+    'article section', 'article section header'
+  ].forEach((selector) => assert(selector, true));
+
+  [
+    'div span', 'article span', 'article div header'
+  ].forEach((selector) => assert(selector, false));
+
+  t.end();
+});
+
+test('IDs', (t) => {
+  [
+    '#bare',
+    'article#on-semantic',
+    'div#on-non-semantic'
+  ].forEach((selector) => assert(selector, false));
+
+  t.end();
+});
+
+test('class names', (t) => {
+  [
+    '.bare',
+    'article.on-semantic',
+    'div.on-non-semantic'
+  ].forEach((selector) => assert(selector, false));
+
+  t.end();
+});
+
+test('attributes', (t) => {
+  [
+	'[role="button"]',
+	'section[role="dialog"]'
+  ].forEach((selector) => assert(selector, true));
+
+  [
+	'[id]',
+	'[id="foo"]',
+	'[data-anything]',
+	'[data-anything="with value"]',
+	'[role]',
+	'[role="unknown"]',
+	'div[role="dialog"]'
+  ].forEach((selector) => assert(selector, false));
+
+  t.end();
+});
